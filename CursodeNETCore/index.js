efa2223abcf4409619c4726bc9e591305d135146
John Doe <johndoe23@example.com>
2021-02-04 15:23:03 -0600

    Archivos javacript añadidos

--- CursodeNETCore/index.js
+++ CursodeNETCore/index.js
@@ -1,10 +1,6 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script>
-			$(document).ready(function () {
-				$('#btnClic').on('click', AlertaClic);
-			});
-
-			function AlertaClic() {
-				alert('Haz hecho clic');
-			}
-		</script>
+$(document).ready(function () {
+	$('#btnClic').on('click', AlertaClic);
+});
+function AlertaClic() {
+	alert('Haz hecho clic');
+}
